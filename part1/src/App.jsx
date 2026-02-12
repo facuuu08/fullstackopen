c36069c9b9ed29a2680e157ea0cccec0653898e3
facuuu08 <facugabiglesias@gmail.com>
2026-02-12 11:14:04 -0300

    Update App.jsx
    Add exercise 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -24,26 +24,31 @@ const Total = (props) => {
   return <p>Number of exercises {props.total}</p>
 }
 
-const App = () => {
+const App = (props) => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content 
-        p1={part1} ex1={exercises1} 
-        p2={part2} ex2={exercises2} 
-        p3={part3} ex3={exercises3} 
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
-    </div>
-  )
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+ return (
+   <div>
+    <Header course={course} />
+    <Content
+      p1={part1.name} ex1={part1.exercises}
+      p2={part2.name} ex2={part2.exercises}
+      p3={part3.name} ex3={part3.exercises}
+    />
+    <Total total={part1.exercises + part2.exercises + part3.exercises} />
+   </div>
+ )
 }
 
 export default App
