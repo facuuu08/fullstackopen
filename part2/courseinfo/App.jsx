c9503cf9160e1cabd96ca8bd05c0e37615f66bde
facuuu08 <facugabiglesias@gmail.com>
2026-02-14 17:54:21 -0300

    Update App.jsx
    Add app jsx (exercise 2.5)

--- part2/courseinfo/App.jsx
+++ part2/courseinfo/App.jsx
@@ -1,25 +1,4 @@
-const Header = ({course}) => <h1>{course.name}</h1>
-
-const Part = ({part}) => <p>{part.name} {part.exercises}</p>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </div>
-  )
-
-const Total = ({parts}) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <b>total of {total} exercises</b>
-}
-
-const Course = ({course}) => (
-  <div>
-   <Header course={course} />
-   <Content parts={course.parts} />
-   <Total parts={course.parts} />
-  </div>
-)
+import Course from './Course'
 
 const App = () => {
   const courses = [
