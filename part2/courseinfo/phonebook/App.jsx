f3f32f05e0e594a3dcf44f4555947ec985761046
facuuu08 <facugabiglesias@gmail.com>
2026-02-15 10:51:04 -0300

    Create App.jsx
    Add exercise 2.6

---
+++ part2/courseinfo/phonebook/App.jsx
@@ -0,0 +1,48 @@
+import {useState} from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+  "use strict";
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+     <h2>Phonebook</h2>
+     <form onSubmit={addPerson}>
+       <div>
+        name: <input value={newName} onChange={handleNameChange} />
+       </div>
+       <div>
+        <button type="submit">add</button>
+       </div>
+     </form>
+      <div>debug: {newName}</div>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
